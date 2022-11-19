c32920713d21b1772cfeaa72de2a173c86caad1e
yashguptaFE <yash.gupta@smallcase.com>
2022-11-19 15:48:58 +0530

    ready to launch. Let's gogit add .git add .

---
+++ src/CardList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = ({ robots }) => {
+    return (
+        <div>
+            {
+                robots.map((robot) => <Card key={robot.id} id={robot.id} name={robot.name} email={robot.email} />)
+            }
+        </div>
+    );
+}
+
+export default CardList;
