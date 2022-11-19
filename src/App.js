b77a326d3a70d121e8c6b48b2efc0941f5244369
yashguptaFE <yash.gupta@smallcase.com>
2022-11-19 16:29:08 +0530

    feat: Integrated JSON API to fetch robo data and added scroll HOC for CardList

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import {robots} from './robots';
+import Scroll from "./Scroll";
 import './App.css'
 
 class App extends React.Component{
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        return response.json();
+      })
+      .then(users => this.setState({robots: users}))
   }
 
   onSearchChange = (event) => {
@@ -18,16 +26,22 @@ class App extends React.Component{
   }
 
   render() {
-    const filteredRobots = this.state.robots.filter(robot => {
+    const filteredRobots = this.state.robots?.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     })
-    return (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} /> 
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    if(this.state.robots.length === 0) {
+      return <h1 className="tc"><i>Loading...</i></h1>
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} /> 
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+    }
   }
 }
 
