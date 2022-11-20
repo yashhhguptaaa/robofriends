84d2d993a82978fb38f577dc56c3baf7142d5ae0
yashguptaFE <yash.gupta@smallcase.com>
2022-11-21 02:02:53 +0530

    refractor : move all the pure components in components folder and components whichhave states into container folder.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import CardList from "./CardList";
-import SearchBox from "./SearchBox";
-import Scroll from "./Scroll";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
 import './App.css'
 
 class App extends React.Component{
@@ -26,13 +26,13 @@ class App extends React.Component{
   }
 
   render() {
-    const filteredRobots = this.state.robots?.filter(robot => {
-      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots?.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
-    if(this.state.robots.length === 0) {
-      return <h1 className="tc"><i>Loading...</i></h1>
-    } else {
-      return (
+    return !robots.length ? 
+    <h1 className="tc"><i>Loading...</i></h1> :
+    (
         <div className="tc">
           <h1 className="f1">RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange} /> 
@@ -40,8 +40,7 @@ class App extends React.Component{
             <CardList robots={filteredRobots} />
           </Scroll>
         </div>
-      );
-    }
+    );
   }
 }
 
